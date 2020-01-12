41362a3c10fa56d23392915f877c761363dfd4c3
HCDesktop <HaydenLCrossJunk@gmail.com>
2020-01-11 18:49:18 -0800

    added partial prompt functionality

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
 const questions = [
-  
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub Username"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"],
+    }
 ];
 
+
+inquirer
+    .prompt(/* {
+        type: "input",
+        name: "username",
+        message: "Enter you GitHub username"
+        
+    },
+    {
+        type:"list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"],
+    }  */
+    questions
+    )
+    .then(function({ username }, { color }) { 
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+        axios
+        .get(queryUrl)
+        .then(function(res) {
+            try{
+                const repoData = res.data;
+                repoData.forEach(function(repo) {
+                    console.log(repo.name)
+                })
+            }catch(err){
+                throw err;
+            }
+        });
+    });
+
+
+
+
+
+/*
 function writeToFile(fileName, data) {
  
 }
 
 function init() {
 
-init();
+init(); */
+
