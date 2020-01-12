260b682cc4f03f5cbc874fcdd8e28a0fe52266ce
HCDesktop <HaydenLCrossJunk@gmail.com>
2020-01-11 20:09:12 -0800

    undefined issues

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
+const generateHTML = require('./generateHTML.js')
+
 const questions = [
     {
         type: "input",
@@ -16,49 +18,48 @@ const questions = [
     }
 ];
 
+var data = {};
+
+function init() {
 
 inquirer
-    .prompt(/* {
-        type: "input",
-        name: "username",
-        message: "Enter you GitHub username"
-        
-    },
-    {
-        type:"list",
-        name: "color",
-        message: "What is your favorite color?",
-        choices: ["green", "blue", "pink", "red"],
-    }  */
+    .prompt(
     questions
     )
-    .then(function({ username }, { color }) { 
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
+    .then(function({ username }){ 
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+        
         axios
         .get(queryUrl)
         .then(function(res) {
             try{
-                const repoData = res.data;
-                repoData.forEach(function(repo) {
-                    console.log(repo.name)
-                })
+                const resData = res.data;
+                    resData.forEach(function(res){
+                        var data = res.data;
+                 
+                    console.log(data.username,
+                     data.public_repos,
+                     data.location,
+                     data.bio);
+                    });
+                    
+                
             }catch(err){
                 throw err;
             }
         });
     });
-
-
-
-
-
-/*
-function writeToFile(fileName, data) {
- 
 }
 
-function init() {
 
-init(); */
+
+
+
+//function writeToFile(fileName, data) {
+ 
+//}
+
+
+init(); 
 
